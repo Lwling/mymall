843d6bccac9d342db96f5f5f980d2287d59f8154
Lwling <x071@qq.com>
2018-05-31 13:29:42 +0800

    用脚手架搭建一个基于webpack的vue项目

---
+++ src/components/carousel.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="carousel">
+        <swiper :options="swiperOption" ref="mySwiper">
+        <swiper-slide><img src="../assets/2.jpg" alt=""></swiper-slide>
+        <swiper-slide>2</swiper-slide>
+        <swiper-slide>3</swiper-slide>
+        <swiper-slide>4</swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+</template>
+<script>
+import "swiper/dist/css/swiper.css";
+import {swiper,swiperSlide} from "vue-awesome-swiper";
+export default {
+    name:"carousel",
+    components:{
+        swiper,
+        swiperSlide
+    },
+    data(){
+        return{
+            swiperOption:{
+            pagination:{
+                el:".swiper-pagination",
+                dynamicBullets:true
+            },
+            autoplay:{
+                delay:2500,
+                disableOnInteraction:false
+            }
+        }
+    };
+},
+computed:{
+    swiper() {
+        return this.$refs.mySwiper.swiper;
+    }
+},
+mounted(){
+    console.log("this is current instance object",this.swiper);
+    this.swiper.slideTo(3,100,false);
+    }
+};
+</script>
+<style scoped>
+.carousel{
+    background:#F7D1D4;
+}
+</style>
+
+
---
+++ src/components/myHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="myHeader">
+    <img src="../assets/suger.png" alt="">
+    <h1>Sugar Store</h1>
+  </div>
+</template>
+<script>
+export default {
+  name:"myHeader",
+  data(){
+    return{
+      msg:"Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+<style scoped>
+.myHeader{
+  width:100%;
+  height: 75px;
+  background-color:#F3758E;
+  color:antiquewhite;
+}
+img{
+  float: left;
+  width: 75px;
+  height: 75px;
+  clear: both;
+}
+h1{
+  float: left;
+}
+</style>
+
+
